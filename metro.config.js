5efd061203e93647da54fc56bda500d4ab10553f
Vidit <vidit.viionn@gmail.com>
2024-02-02 18:59:14 +0530

    changes for android

--- metro.config.js
+++ metro.config.js
@@ -7,5 +7,9 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  * @type {import('metro-config').MetroConfig}
  */
 const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = {
+    resolver: {
+      sourceExts: ['jsx', 'js', 'ts', 'tsx'], // Include 'ts' and 'tsx'
+    },
+  };
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
